29000f8d554f49adb1ecefff32d1f7d2362b2723
LU SIO <949002090@qq.com>
2023-06-17 00:55:58 +0800

    TEST Initial commit

--- AllException/PermissiondeniedException.cs
+++ AllException/PermissiondeniedException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace UnoHttp
-{
+{ 
 	internal class PermissiondeniedException : Exceptioninterface
 	{
 		internal PermissiondeniedException(Dictionary<String, Object> errorData) : base(errorData)
